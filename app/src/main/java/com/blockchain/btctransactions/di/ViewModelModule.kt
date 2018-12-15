22c4cb4a4fa32d6e76f79fda05e78f6ffd6fff09
Antonis Latas <>
2018-12-15 11:19:49 +0000

    Add arch lifecycle components Add viewmodel factory and transactions viewmodel

---
+++ app/src/main/java/com/blockchain/btctransactions/di/ViewModelModule.kt
@@ -0,0 +1,20 @@
+package com.blockchain.btctransactions.di
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.blockchain.btctransactions.ui.TransactionsViewModel
+import com.blockchain.btctransactions.viewmodel.AppViewModelFactory
+import dagger.Binds
+import dagger.Module
+import dagger.multibindings.IntoMap
+
+@Module
+abstract class ViewModelModule {
+    @Binds
+    @IntoMap
+    @ViewModelKey(TransactionsViewModel::class)
+    abstract fun bindTransactionsViewModel(userViewModel: TransactionsViewModel): ViewModel
+
+    @Binds
+    abstract fun bindViewModelFactory(factory: AppViewModelFactory): ViewModelProvider.Factory
+}
---
+++ app/src/main/java/com/blockchain/btctransactions/ui/TransactionsViewModel.kt
@@ -0,0 +1,4 @@
+package com.blockchain.btctransactions.ui
+
+class TransactionsViewModel {
+}
